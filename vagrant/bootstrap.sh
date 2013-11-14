2c04cda0c68c131c17e5f8d4dc8c7510a5fa61c3
Benjamin Roberts <benjamin.roberts@westnet.com.au>
2013-11-14 13:58:05 +1100

    rolled back to 13.04 to avoid linux-3.11/virtualbox incompatibility

--- vagrant/bootstrap.sh
+++ vagrant/bootstrap.sh
@@ -1,23 +1,22 @@
 #!/bin/bash
 #Generate missing locales
-sudo locale-gen en_AU.UTF-8
+locale-gen en_AU.UTF-8
 
 #Bring the system and its package lists up to date (excluding grub due to manual intervention)
-sudo apt-get -y update
-sudo apt-mark hold grub-pc
-sudo apt-get -y upgrade
+apt-get -y update
+apt-mark hold grub-pc
+apt-get -y upgrade
 
-#Get packages available on apt and cleanup
-sudo apt-get -y install python3 python3-pip postgresql-9.1 nginx rabbitmq-server swift postgresql-server-dev-9.1 python3-dev
-sudo apt-get -y autoremove
-
-#Setup python3 aliases and load them for use in rest of script
-echo "alias python=\"python3\"
-alias pip=\"pip3\"" >> ~/.bashrc
-source ~/.bashrc
+#Get packages available on apt
+apt-get -y install python3 python3-pip postgresql-9.1 nginx rabbitmq-server swift postgresql-server-dev-9.1 python3-dev
+apt-get -y autoremove
 
 #Install python packages
-sudo pip3 install Django Django-Celery psycopg2 Pillow
+pip3 install Django Django-Celery psycopg2 Pillow
+
+#Setup python3 aliases for vagrant user
+echo "alias python=\"python3\"
+alias pip=\"pip3\"" >> /home/vagrant/.bashrc
 
 #Configure postgres (add user for vagrant and user/database for django)
 sudo -u postgres bash -c "createuser -s vagrant
