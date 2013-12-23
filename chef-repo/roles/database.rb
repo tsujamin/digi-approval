caa3a12f84f6aea722f72cdeb95932882c5e53aa
Daniel Axtens <daniel@axtens.net>
2013-12-24 03:02:36 +0800

    Install postgresql 9.3

--- chef-repo/roles/database.rb
+++ chef-repo/roles/database.rb
@@ -1,15 +1,18 @@
 name "database"
 description "Pure Database Server"
 
-default_attributes 'postgresql' => { 'config_pgtune' => {'dbtype' => "web"} }
+default_attributes 'postgresql' => { 'config_pgtune' => {'dbtype' => "web"},
+                                     'version' => "9.3"
+                                   }
 # Password is set in environment (e.g. development.json)
 
-run_list "recipe[postgresql::server]",
-    "recipe[postgresql::config_initdb]",
-    "recipe[postgresql::config_pgtune]",
-    "recipe[django_db]",
-    # for debugging
-    "recipe[postgresql::client]"
+run_list "recipe[postgresql::yum_pgdg_postgresql]",
+         "recipe[postgresql::server]",
+         "recipe[postgresql::config_initdb]",
+         "recipe[postgresql::config_pgtune]",
+         "recipe[django_db]",
+         # for debugging
+         "recipe[postgresql::client]"
 
 
 
