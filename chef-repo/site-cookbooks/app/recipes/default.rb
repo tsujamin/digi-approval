e41da4a68f121005e19220be6a980ecf6288445f
Daniel Axtens <daniel@axtens.net>
2013-12-29 04:17:54 +0800

    Use system Python27 packages.

--- chef-repo/site-cookbooks/app/recipes/default.rb
+++ chef-repo/site-cookbooks/app/recipes/default.rb
@@ -7,38 +7,46 @@
 # All rights reserved - Do Not Redistribute
 #
 
-# Install Python3
-# this messily hopes the attributes are correctly set elsewhere
-# for now in the role.
-if not ::File.exists?("#{node["python_build"]["install_prefix"]}/bin/python2.7")
-  include_recipe "python-build"
-end
-
 # Verify that we're a messy dev env only
 if node.chef_environment != "development" 
   abort "No production environment defined yet, only 'development'"
 end
 
+# Install Python
+# install the software collections, then install the python 2.7
+# package
+include_recipe "python::virtualenv"
+yum_package "centos-release-SCL"
+yum_package "python27"
+# Allow it to find postgres
+ENV["PATH"] = ENV["PATH"] + ":/usr/pgsql-9.3/bin"
+
 # do a simple dodgy deployment
 # heavily based on https://github.com/poise/application_python/blob/master/providers/django.rb
 
 # create a virtualenv
+old_ld_library_path = ENV["LD_LIBRARY_PATH"]
+ENV["LD_LIBRARY_PATH"] = "/opt/rh/python27/root/usr/lib64/"
 python_virtualenv "/vagrant/env" do
-  interpreter "python2.7"
+  interpreter "/opt/rh/python27/root/usr/bin/python"
   owner "vagrant"
   group "vagrant"
   action :create
 end
+ENV["LD_LIBRARY_PATH"] = old_ld_library_path
 
 # install packages
 # (make sure git is around for our own versions of packages)
 include_recipe 'git'
-# it is enough to use the right pip to get a correctly located install
+
 Chef::Log.info("Installing packages from requirements.txt")
-execute "/vagrant/env/bin/pip install -r requirements.txt" do
+# interestingly using the absolute path to virtualenv pip will make
+# sure things are installed to the virtualenv
+execute "scl enable python27 '/vagrant/env/bin/pip install -r requirements.txt'" do
   cwd "/vagrant/src"
   user "vagrant"
   group "vagrant"
+  # so pip can write log files, build directories, etc.
   environment 'HOME' => '/home/vagrant/'
 end
 
@@ -62,7 +70,7 @@ template "/tmp/init_django.bash" do
   variables node['digiactive']
 end
 
-execute "/tmp/init_django.bash" do
+execute "scl enable python27 '/tmp/init_django.bash'" do
   cwd "/vagrant/src/digiapproval_project"
   user "vagrant"
   group "vagrant"
