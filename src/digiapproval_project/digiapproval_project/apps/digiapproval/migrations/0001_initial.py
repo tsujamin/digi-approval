1435d867a7e4dd2e4478dd4a5053b6f12efc7f65
Daniel Axtens <daniel@axtens.net>
2014-01-17 11:15:24 +0800

    remove old migrations, replace with new migration

--- src/digiapproval_project/digiapproval_project/apps/digiapproval/migrations/0001_initial.py
+++ src/digiapproval_project/digiapproval_project/apps/digiapproval/migrations/0001_initial.py
@@ -8,6 +8,53 @@ from django.db import models
 class Migration(SchemaMigration):
 
     def forwards(self, orm):
+        # Adding model 'UserFile'
+        db.create_table(u'digiapproval_userfile', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length=255)),
+            ('_file', self.gf('django.db.models.fields.files.FileField')(max_length=100)),
+            ('virus_status', self.gf('django.db.models.fields.CharField')(default='UNSCANNED', max_length=16)),
+        ))
+        db.send_create_signal(u'digiapproval', ['UserFile'])
+
+        # Adding model 'CustomerAccount'
+        db.create_table(u'digiapproval_customeraccount', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('user', self.gf('django.db.models.fields.related.OneToOneField')(to=orm['auth.User'], unique=True)),
+            ('account_type', self.gf('django.db.models.fields.CharField')(default='CUSTOMER', max_length=16)),
+        ))
+        db.send_create_signal(u'digiapproval', ['CustomerAccount'])
+
+        # Adding M2M table for field parent_accounts on 'CustomerAccount'
+        m2m_table_name = db.shorten_name(u'digiapproval_customeraccount_parent_accounts')
+        db.create_table(m2m_table_name, (
+            ('id', models.AutoField(verbose_name='ID', primary_key=True, auto_created=True)),
+            ('from_customeraccount', models.ForeignKey(orm[u'digiapproval.customeraccount'], null=False)),
+            ('to_customeraccount', models.ForeignKey(orm[u'digiapproval.customeraccount'], null=False))
+        ))
+        db.create_unique(m2m_table_name, ['from_customeraccount_id', 'to_customeraccount_id'])
+
+        # Adding model 'WorkflowSpec'
+        db.create_table(u'digiapproval_workflowspec', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('name', self.gf('django.db.models.fields.CharField')(max_length='64')),
+            ('owner', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['auth.Group'])),
+            ('public', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('spec', self.gf('digiapproval_project.apps.digiapproval.fields.WorkflowSpecField')()),
+        ))
+        db.send_create_signal(u'digiapproval', ['WorkflowSpec'])
+
+        # Adding model 'Workflow'
+        db.create_table(u'digiapproval_workflow', (
+            (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
+            ('customer', self.gf('django.db.models.fields.related.ForeignKey')(related_name='workflow_customer', to=orm['digiapproval.CustomerAccount'])),
+            ('approver', self.gf('django.db.models.fields.related.ForeignKey')(related_name='workflow_approver', to=orm['auth.User'])),
+            ('workflow', self.gf('digiapproval_project.apps.digiapproval.fields.WorkflowField')()),
+            ('completed', self.gf('django.db.models.fields.BooleanField')(default=False)),
+            ('spec', self.gf('django.db.models.fields.related.ForeignKey')(to=orm['digiapproval.WorkflowSpec'])),
+        ))
+        db.send_create_signal(u'digiapproval', ['Workflow'])
+
         # Adding model 'Task'
         db.create_table(u'digiapproval_task', (
             (u'id', self.gf('django.db.models.fields.AutoField')(primary_key=True)),
@@ -18,6 +65,21 @@ class Migration(SchemaMigration):
 
 
     def backwards(self, orm):
+        # Deleting model 'UserFile'
+        db.delete_table(u'digiapproval_userfile')
+
+        # Deleting model 'CustomerAccount'
+        db.delete_table(u'digiapproval_customeraccount')
+
+        # Removing M2M table for field parent_accounts on 'CustomerAccount'
+        db.delete_table(db.shorten_name(u'digiapproval_customeraccount_parent_accounts'))
+
+        # Deleting model 'WorkflowSpec'
+        db.delete_table(u'digiapproval_workflowspec')
+
+        # Deleting model 'Workflow'
+        db.delete_table(u'digiapproval_workflow')
+
         # Deleting model 'Task'
         db.delete_table(u'digiapproval_task')
 
@@ -59,6 +121,13 @@ class Migration(SchemaMigration):
             'model': ('django.db.models.fields.CharField', [], {'max_length': '100'}),
             'name': ('django.db.models.fields.CharField', [], {'max_length': '100'})
         },
+        u'digiapproval.customeraccount': {
+            'Meta': {'object_name': 'CustomerAccount'},
+            'account_type': ('django.db.models.fields.CharField', [], {'default': "'CUSTOMER'", 'max_length': '16'}),
+            u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
+            'parent_accounts': ('django.db.models.fields.related.ManyToManyField', [], {'blank': 'True', 'related_name': "'sub_accounts'", 'null': 'True', 'symmetrical': 'False', 'to': u"orm['digiapproval.CustomerAccount']"}),
+            'user': ('django.db.models.fields.related.OneToOneField', [], {'to': u"orm['auth.User']", 'unique': 'True'})
+        },
         u'digiapproval.task': {
             'Meta': {'object_name': 'Task'},
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
@@ -76,7 +145,7 @@ class Migration(SchemaMigration):
             'Meta': {'object_name': 'Workflow'},
             'approver': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'workflow_approver'", 'to': u"orm['auth.User']"}),
             'completed': ('django.db.models.fields.BooleanField', [], {'default': 'False'}),
-            'customer': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'workflow_customer'", 'to': u"orm['auth.User']"}),
+            'customer': ('django.db.models.fields.related.ForeignKey', [], {'related_name': "'workflow_customer'", 'to': u"orm['digiapproval.CustomerAccount']"}),
             u'id': ('django.db.models.fields.AutoField', [], {'primary_key': 'True'}),
             'spec': ('django.db.models.fields.related.ForeignKey', [], {'to': u"orm['digiapproval.WorkflowSpec']"}),
             'workflow': ('digiapproval_project.apps.digiapproval.fields.WorkflowField', [], {})
