559558e394f5d1d29203e241adf3f910b1aec5d5
Andrew Donnellan <andrew@donnellan.id.au>
2014-01-18 13:53:57 +1100

    Add additional convenience auth decorators

---
+++ src/digiapproval_project/digiapproval_project/apps/digiapproval/auth_decorators.py
@@ -0,0 +1,50 @@
+"""Miscellaneous authentication decorators to make our lives easier.
+
+TODO: Testing!"""
+
+import models
+from django.contrib.auth.decorators import user_passes_test, REDIRECT_FIELD_NAME
+
+def login_required_customeraccount(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, and that
+    the user holds a CustomerAccount. Redirects to login page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and hasattr(u, 'customeraccount'),
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def login_required_customer(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, and that
+    the user holds a CustomerAccount of type CUSTOMER. Redirects to login page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and hasattr(u, 'customeraccount') and u.customeraccount.account_type == 'CUSTOMER',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
+
+
+def login_required_organisation(function=None, redirect_field_name=REDIRECT_FIELD_NAME, login_url=None):
+    """
+    Decorator for views that checks that the user is logged in, and that
+    the user holds a CustomerAccount of type ORGANISATION. Redirects to login page if necessary.
+    """
+    actual_decorator = user_passes_test(
+        lambda u: u.is_authenticated and hasattr(u, 'customeraccount') and u.customeraccount.account_type == 'ORGANISATION',
+        login_url=login_url,
+        redirect_field_name=redirect_field_name
+    )
+    if function:
+        return actual_decorator(function)
+    return actual_decorator
