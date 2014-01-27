ea5122ddd017fb97bc865f07089a4dea24940d09
Daniel Axtens <daniel@axtens.net>
2014-01-27 16:45:06 +1100

    simple init script

---
+++ src/dev/init.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+which tmux || yum install -y tmux
+
+cd `dirname $0`/../..
+
+tmux has-session -t digiapproval && tmux attach -t digiapproval || {
+    tmux new-session -d -s digiapproval "scl enable python27 bash"
+    for rcfile in src/dev/*.rc; do
+        rcname=`basename $rcfile .rc`
+        tmux new-window -t digiapproval -n $rcname "src/dev/spawn-scl.sh ${rcname}.rc"
+    done
+    tmux attach -t digiapproval
+}
