b0438a730b3674cb5b9c15343e79ed28fc866698
Daniel Axtens <daniel@axtens.net>
2014-02-01 14:47:07 +1100

    lamson. Ignore a test file we didn't write.

--- src/digiapproval_project/digimail/config/boot.py
+++ src/digiapproval_project/digimail/config/boot.py
@@ -9,7 +9,7 @@ import jinja2
 logging.config.fileConfig("config/logging.conf")
 
 # the relay host to actually send the final message to
-settings.relay = Relay(host=settings.relay_config['host'], 
+settings.relay = Relay(host=settings.relay_config['host'],
                        port=settings.relay_config['port'], debug=1)
 
 # where to listen for incoming messages
@@ -18,10 +18,9 @@ settings.receiver = SMTPReceiver(settings.receiver_config['host'],
 
 Router.defaults(**settings.router_defaults)
 Router.load(settings.handlers)
-Router.RELOAD=True
-Router.UNDELIVERABLE_QUEUE=queue.Queue("run/undeliverable")
+Router.RELOAD = True
+Router.UNDELIVERABLE_QUEUE = queue.Queue("run/undeliverable")
 
 view.LOADER = jinja2.Environment(
-    loader=jinja2.PackageLoader(settings.template_config['dir'], 
+    loader=jinja2.PackageLoader(settings.template_config['dir'],
                                 settings.template_config['module']))
-
