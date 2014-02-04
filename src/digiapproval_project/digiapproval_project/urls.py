4050ceb124fe5bf8644a36a95072a7768eac240a
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2014-02-04 19:51:19 +1100

    scaffold app

--- src/digiapproval_project/digiapproval_project/urls.py
+++ src/digiapproval_project/digiapproval_project/urls.py
@@ -8,5 +8,6 @@ urlpatterns = patterns(
     url(r'^digiapproval/',
         include('digiapproval_project.apps.digiapproval.urls')),
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^pages/', include('django.contrib.flatpages.urls'))
+    url(r'^pages/', include('django.contrib.flatpages.urls')),
+    url(r'^builder/', include('digiapproval_project.apps.spec_builder.urls')),
 )
