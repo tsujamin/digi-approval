e388c4da4079d25966f903be35ee38309ef6ff85
Benjamin Roberts <Benjamin@BGRoberts.id.au>
2014-02-05 13:18:25 +1100

    toggle spec visability

--- src/digiapproval_project/digiapproval_project/apps/spec_builder/views.py
+++ src/digiapproval_project/digiapproval_project/apps/spec_builder/views.py
@@ -10,6 +10,14 @@ def index(request):
 @login_required_super
 def builder_home(request):
     """Shows a list of all system workflow specs (organised by owner) with ability to view/disable them"""
+    if request.method == 'POST':
+        spec = get_object_or_404(approval_models.WorkflowSpec, 
+                                id=request.POST.get('spec', -1))
+        if 'toggle_public' in request.POST:
+            spec.public = not spec.public
+            spec.save()
+        elif 'edit' in request.POST:
+            raise NotImplementedError
     wf_spec_ordered = {} #{owner: [specs],...}
     for spec in approval_models.WorkflowSpec.objects.all():
         if spec.owner.name not in wf_spec_ordered: wf_spec_ordered[spec.owner.name] = []
