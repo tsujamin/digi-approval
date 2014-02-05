141d686702e9df97dce02d596435a221a2e84e16
Daniel Axtens <daniel@axtens.net>
2014-02-06 08:20:54 +1100

    script to build static assets [skip ci]

---
+++ src/dev/build_static_assets.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+
+cd `dirname $0`  # src/dev
+echo "Making docs..."
+cd ../../docs/website
+python makedocs.py
+echo "done"
+echo "Load the docs with syncdb."
+echo ""
+
+echo "Lessifying every less file in apps/digiapproval/static/less"
+cd ../../src/digiapproval_project/digiapproval_project/apps/digiapproval/static/less
+for lessfile in $(find . -name "*.less"); do
+    lessname=`basename $lessfile .less`
+    lessdir=`dirname $lessfile`
+    echo "Converting $lessdir/$lessname.less"
+    lessc $lessdir/$lessname > ../css/$lessdir/$lessname.css
+done
+echo "done"
+echo "Don't forget to collectstatic if deploying with uWSGI."
+echo ""
